77f592158022c71a4b3b12d57c3f3f2723a33cab
treylee <trieveon.lee@gmail.com>
2025-09-07 12:31:08 -0700

    qr code bizz non tested implementation

---
+++ BizzPortal/QRComponents/BizzQRScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BizzQRScannerView.swift
+//  vibeIn
+//
+//  Created by Trieveon Cooper on 9/7/25.
+//
+
+import SwiftUI
+
+struct BizzQRScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BizzQRScannerView()
+}
